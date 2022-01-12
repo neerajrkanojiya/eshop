140d7598f9dd273e6125dc4b1fee601103555b86
neerajrkanojiya <34946903+neerajrkanojiya@users.noreply.github.com>
2022-01-12 12:04:07 +0530

    Staging with firebase issue

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import './App.css';
+import Home from "./Home";
+import Header from "./Header";
+import Checkout from "./Checkout";
+import Login from "./Login";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Switch } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">    
+    <div className="App">
+      <Router>
+
+        <Switch>
+
+          <Route path="/login">
+            <Login />
+          </Route>
+
+          <Route path="/checkout">
+            <Header />
+            <Checkout />
+          </Route>
+
+          <Route path="/">    
+            <Header />
+            <Home />
+          </Route>
+
+        </Switch>
+
+      </Router>
+
     </div>
   );
 }
